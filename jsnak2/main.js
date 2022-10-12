c60f7f6b2cd1387733fe8855d44b3a052e0f28fb
federossi93 <rossifederico0993@gmail.com>
2022-10-12 12:11:16 +0200

    end

---
+++ jsnak2/main.js
@@ -0,0 +1,14 @@
+//L’utente inserisce due parole in successione, con due prompt.
+
+let parola1 = prompt('inserisci parola')
+
+let parola2 = prompt('inserisci altra parola')
+
+//Il software stampa prima la parola più corta, poi la parola più lunga.
+
+
+if(parola1 > parola2){
+    prompt(parola1 + ' ' + ' ' + parola2)
+}else{
+    prompt(parola2 + ' ' + ' ' + parola1)
+}
